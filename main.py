f8114de1036a1515b64035e7d2b3f17ddad5789f
kn0wleadge <firsovghenya@gmail.com>
2024-11-19 17:38:37 +0300

    creating simple dataframe using first 58 values from binary data array

--- main.py
+++ main.py
@@ -1,35 +1,85 @@
 #include <iostream>
 import numpy as np
 import pandas as pd
+from columns import dict
+from IPython.display import display, HTML
+# dt = np.dtype([ day"  ,  hour", "i2"),  minute", "i2"),  second"  ,  year"  , \
+#                  geodetic latitude"  ,  geographic longitude"  , \
+#                  altitude"  ,  geographic latitude*"  ,  geographic longitude*"  , \
+#                  corrected geomagnetic latitude"  ,  corrected geomagnetic longitude"  , \
+#                  hour of magnetic local time"  ,  minute of hour of magnetic local time"  , \
+#                  second of minute of magnetic local time"  , \
+#                  hour of day for 1st second of data"  , \
+#                  minute of hour for 1st second of data"  , \
+#                  second of minute for 1st second of data", "i2"), \
+#                  channel 4, 9450 eV electrons,"  ,  channel 3, 13900 eV electrons,"  , \
+#                  channel 2, 20400 eV electrons,"  ,  Channel 1, 30000 eV electrons"  ,  channel 8, 2040 eV electrons"  , \
+#                  channel 7, 3000 eV electrons"  ,  channel 6, 4400 eV electrons"  , \
+#                  channel 5, 6460 eV electrons"  ,  channel 12, 646 eV electrons"  , \
+#                  channel 11, 949 eV electrons/status word 1 if SSJ5 data"  , 
+#                  channel 10, 949 eV electrons"  ,  channel 9, 1392 eV electrons"  , \
+#                  channel 16, 139 eV electrons"  ,  channel 15, 204 eV electrons"  , \
+#                  channel 14, 300 eV electrons"  ,  channel 13, 440 eV electrons,"  , \
+#                  channel 20, 30 eV electrons"  , \
+#                  channel 19, 44 eV electrons"  ,  channel 18, 65 eV electrons"  , \
+#                  channel 17, 95 eV electrons"  ,  channel 4, 9450 eV ions2"  ,  channel 3, 13900 eV ions 2"  , \
+#                  channel 2, 20400 eV ions 2,"  ,  channel 1, 30000 eV ions 2"  , \
+#                  channel 8, 2040 eV ions 2"  ,  channel 7, 3000 eV ions 2"  ,  channel 6, 4400 eV ions 2"  , \
+#                  channel 5, 6460 eV ions 2"  ,  channel 12, 646 eV ions 2"  ,  channel 11, 949 eV ions2"  , \
+#                  channel 10, 949 eV ions2"  ,  channel 9, 1392 eV ions2"  , \
+#                  channel 16, 139 eV ions2"  ,  channel 15, 204 eV ions2"  , \
+#                  channel 14, 300 eV ions2"  ,  channel 13, 440 eV ions2"  , \
+#                  channel 20, 30 eV ions2"  , \
+#                  channel 14, 300 eV ions3"  ,  channel 13, 440 eV ions3"  , \
+#                  channel 20, 30 eV ions3"  ,  channel 19, 44 eV ions2"  , \
+#                  channel 18, 65 eV ions2"  ,  channel 17, 95 eV ions2"  ])
+#     columns_names = np.array(['day of year', 'Hour of day', 'Minute of hour', \
+#                               'Second of minute', 'Integer year', 'Geodetic latitude', 'Geographic longitude', \
+#                                 'Altitude', 'Geographic latitude at 110 km altitude' , \
+#                                     'Geographic longitude at 110 km altitude', 
+#                                     "corrected geomagnetic latitude", "corrected geomagnetic longitude", \
+#                  "hour of magnetic local time", "minute of hour of magnetic local time", \
+#                  "second of minute of magnetic local time", \
+#                  "hour of day for n second of data", \
+#                  "minute of hour for n second of data",\
+#                  "second of minute for n second of data", "channel 4, 9450 eV electrons,", "channel 3, 13900 eV electrons, \
+# #                 channel 2, 20400 eV electrons, Channel 1, 30000 eV electrons", "channel 8, 2040 eV electrons", \
+#                   "channel 7, 3000 eV electrons",  "channel 6, 4400 eV electrons"  , \
+#                   "channel 5, 6460 eV electrons"  ,  "channel 12, 646 eV electrons"  , \
+#                   "channel 11, 949 eV electrons/status word 1 if SSJ5 data"  , 
+#                   "channel 10, 949 eV electrons"  ,  "channel 9, 1392 eV electrons"  , \
+#                   "channel 16, 139 eV electrons"  ,  "channel 15, 204 eV electrons"  , \
+#                   "channel 14, 300 eV electrons"  ,  "channel 13, 440 eV electrons,"  , \
+#                   "channel 20, 30 eV electrons"  , \
+#                   "channel 19, 44 eV electrons"  ,  "channel 18, 65 eV electrons"  , \
+#                   "channel 17, 95 eV electrons"  ,  "channel 4, 9450 eV ions2"  ,  "channel 3, 13900 eV ions 2"  , \
+#                   "channel 2, 20400 eV ions 2,"  ,  "channel 1, 30000 eV ions 2"  , \
+#                   "channel 8, 2040 eV ions 2"  ,  "channel 7, 3000 eV ions 2"  ,  "channel 6, 4400 eV ions 2"  , \
+#                   "channel 5, 6460 eV ions 2"  ,  "channel 12, 646 eV ions 2"  ,  "channel 11, 949 eV ions2"  , \
+#                   "channel 10, 949 eV ions2"  ,  "channel 9, 1392 eV ions2"  , \
+#                   "channel 16, 139 eV ions2"  ,  "channel 15, 204 eV ions2"  , \
+#                   "channel 14, 300 eV ions2"  ,  "channel 13, 440 eV ions2"  , \
+#                   "channel 20, 30 eV ions2"  , \
+#                   "channel 14, 300 eV ions3"  ,  "channel 13, 440 eV ions3"  , \
+#                   "channel 20, 30 eV ions3"  ,  "channel 19, 44 eV ions2"  , \
+#                   "channel 18, 65 eV ions2"  ,  "channel 17, 95 eV ions2"  ])
+# file = "/home/eugene/Study/kmod/rgr/f15/ssj/1999/12/j4f1599351" 
+# data = np.fromfile(file, dtype=dt)
+# df = pd.DataFrame(data) 
+with open('/home/eugene/Study/kmod/rgr/f15/ssj/1999/12/j4f1599351', 'rb') as f:
 
-dt = np.dtype([("day","i2"), ("hour", "i2"), ("minute", "i2"), ("second","i2"), ("year","i2"), \
-                ("geodetic latitude","i2"), ("geographic longitude","i2"), \
-                ("altitude","i2"), ("geographic latitude*","i2"), ("geographic longitude*","i2"), \
-                ("corrected geomagnetic latitude","i2"), ("corrected geomagnetic longitude","i2"), \
-                ("hour of magnetic local time","i2"), ("minute of hour of magnetic local time","i2"), \
-                ("second of minute of magnetic local time","i2"), \
-                ("hour of day for 1st second of data","i2"), \
-                ("minute of hour for 1st second of data","i2"), \
-                ("second of minute for 1st second of data", "i2"), \ 
-                ("channel 4, 9450 eV electrons,","i2"), ("channel 3, 13900 eV electrons,","i2"), \
-                ("channel 2, 20400 eV electrons,","i2"), ("Channel 1, 30000 eV electrons","i2"), ("channel 8, 2040 eV electrons","i2"), \
-                ("channel 7, 3000 eV electrons","i2"), ("channel 6, 4400 eV electrons","i2"), \
-                ("channel 5, 6460 eV electrons","i2"), ("channel 12, 646 eV electrons","i2"), \
-                ("channel 11, 949 eV electrons/status word 1 if SSJ5 data","i2"), 
-                ("channel 10, 949 eV electrons","i2"), ("channel 9, 1392 eV electrons","i2"), \
-                ("channel 16, 139 eV electrons","i2"), ("channel 15, 204 eV electrons","i2"), \
-                ("channel 14, 300 eV electrons","i2"), ("channel 13, 440 eV electrons,","i2"), \
-                ("channel 20, 30 eV electrons","i2"), \
-                ("channel 19, 44 eV electrons","i2"), ("channel 18, 65 eV electrons","i2"), \
-                ("channel 17, 95 eV electrons","i2"), ("channel 4, 9450 eV ions","i2"), ("channel 3, 13900 eV ions","i2"), \
-                ("channel 2, 20400 eV ions,","i2"), ("channel 1, 30000 eV ions","i2"), \
-                ("channel 8, 2040 eV ions","i2"), ("channel 7, 3000 eV ions","i2"), ("channel 6, 4400 eV ions","i2"), \
-                ("channel 5, 6460 eV ions","i2"), ("channel 12, 646 eV ions","i2"), ("channel 11, 949 eV ions","i2"), \
-                ("channel 10, 949 eV ions","i2"), ("channel 9, 1392 eV ions","i2"), \
-                ("channel 16, 139 eV ions","i2"), ("channel 15, 204 eV ions","i2"), \
-                ("channel 14, 300 eV ions","i2"), ("channel 13, 440 eV ions","i2"), \
-                ("channel 20, 30 eV ions","i2"), \
-                ("channel 14, 300 eV ions","i2"), ("channel 13, 440 eV ions","i2"), \
-                ("channel 20, 30 eV ions","i2"), ("channel 19, 44 eV ions","i2"), \
-                ("channel 18, 65 eV ions","i2"), ("channel 17, 95 eV ions","i2"), \
-                ("channel 14, 300 eV ions","i2"), ("channel 13, 440 eV ions","i2")])
+    array  = np.fromfile(f, dtype = np.uint16)
+    print(array.size)
+    j = 0
+    for i in range(array.size):
+
+        if array[i] == 0:
+            j = j+1
+    print(j)
+    keys = list(dict.keys())
+    new_data = {k: [array[i]] for i, k in enumerate(keys)}
+    for i in range(58):
+        new_data[list(new_data.keys())[i]] = array[i]
+    df = pd.DataFrame(new_data) 
+    display(df)
+        
